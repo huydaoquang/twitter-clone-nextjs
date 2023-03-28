d0db392352474c3c8c2e814e10d88275b71bae37
huy dao <you@example.com>
2023-03-28 11:40:21 +0700

    update auth ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-import Layout from "@/components/Layout";
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import Layout from '@/components/Layout';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <>
+      <LoginModal />
+      <RegisterModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
