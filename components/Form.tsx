07dc2b472aa854db3e25c0e64ebc2d1d9efbf241
huy dao <you@example.com>
2023-04-01 14:41:02 +0700

    updated comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
+import usePost from '@/hooks/usePost';
 import usePosts from '@/hooks/usePosts';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useLoginModal from '@/hooks/useLoginModal';
@@ -26,7 +27,7 @@ const Form: React.FC<FromProps> = ({
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts(postId as string);
-
+  const { mutate: mutatePost } = usePost(postId as string);
   const [body, setBody] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -34,18 +35,24 @@ const Form: React.FC<FromProps> = ({
     try {
       setIsLoading(true);
 
-      await axios.post('/api/posts', { body });
+      const url = isComment
+        ? `/api/comments?postId=${postId}`
+        : '/api/posts';
+
+      await axios.post(url, { body });
 
       toast.success('Tweet Created');
 
       setBody('');
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error('Something went wrong');
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
+
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
       {currentUser ? (
